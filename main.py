80133907f622406edb2292818c25a515bf2b1130
penchekrak <spiand97@gmail.com>
2025-07-06 13:50:28 +0300

    4 spaces

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ screen = pygame.display.set_mode((width, height))
  
 # Game loop.
 while True:
-  screen.fill((0, 0, 0))
+    screen.fill((0, 0, 0))
   
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
   
-  # Update.
-  
-  # Draw.
-  
-  pygame.display.flip()
-  fpsClock.tick(fps)
+    # Update.
+    
+    # Draw.
+    
+    pygame.display.flip()
+    fpsClock.tick(fps)

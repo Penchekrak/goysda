d0f960c4cafb459f0e39d428a7fec80b9822694a
Misha Dobritsyn <mddobritsyn@edu.hse.ru>
2025-07-06 15:17:11 +0300

    GameState update now can place stones and keep track of whose turn is

--- game_state.py
+++ game_state.py
@@ -1,4 +1,10 @@
-from typing import Literal, NamedTuple
+from typing import Literal, NamedTuple, Tuple
+from handle_input import ActionType
+
+
+def snapping_mock(stone, list_of_stones):
+    return stone
+
 
 class Stone(NamedTuple):
     x: int
@@ -9,10 +15,22 @@ class Cloud(NamedTuple):
     x: int
     y: int
 
+
 class GameState:
     def __init__(self, config):
-        self.placed_stones = [Stone(x=100, y=100, color='white'), Stone(x=200, y=200, color='black')]
+        self.placed_stones = []
+        self.player_to_move = 0
         self.cloud_state = [Cloud(x=0, y=0)]
 
-    def update(self, user_input):
-        pass
+
+    def update(self, user_input: Tuple[int, int, bool] = None):
+        action = user_input.get(ActionType.MOUSE_DOWN_LEFT, {})
+        if not action:
+            return 
+
+        x, y, is_magnet = action["x"], action["y"], False
+        new_stone = Stone(x=x, y=y, color=['white', 'black'][self.player_to_move])
+        new_stone = snapping_mock(new_stone, self.placed_stones)
+        self.placed_stones.append(new_stone)
+        self.player_to_move = (self.player_to_move + 1) % 2
+

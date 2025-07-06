5f1b1ec7230580538e9269c73cbd834783df14d4
penchekrak <spiand97@gmail.com>
2025-07-06 15:26:40 +0300

    added clouds

--- rendering.py
+++ rendering.py
@@ -1,9 +1,19 @@
 import pygame
 from utils import colors
 
+def render_clouds(screen, game_state, config):
+    cloud_surface = pygame.image.load(config['cloud_image_path']).convert()
+    cloud_surface = pygame.transform.scale(cloud_surface, (config['cloud_scale'] * cloud_surface.get_width(), config['cloud_scale'] * cloud_surface.get_height()))
+    cloud_state = game_state.cloud_state
+    for cloud in cloud_state:
+        screen.blit(cloud_surface, (cloud.x, cloud.y))
 
 def render(screen, game_state, config):
-    screen.fill(colors.get(config['background_color']))
+    board_display = pygame.Surface((config['board_width'], config['board_height']))
+    render_clouds(screen, game_state, config)
+    board_display.fill(colors.get(config['background_color']))
 
     for placed_stone in game_state.placed_stones:
-        pygame.draw.circle(screen, colors.get(placed_stone.color), (placed_stone.x, placed_stone.y), 10)
+        pygame.draw.circle(board_display, colors.get(placed_stone.color), (placed_stone.x, placed_stone.y), 10)
+    
+    screen.blit(board_display, ((config['width'] - config['board_width']) / 2, (config['height'] - config['board_height']) / 2))
